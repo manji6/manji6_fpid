cbbd0546a19f825dd495d384c19057b4e8f7cb23
manji6 <ryosuke.sawada@gmail.com>
2022-04-18 01:19:38 +0900

    added query param instead mode

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,make_response,redirect,url_for,flash
+from flask import Flask,render_template,request,make_response,redirect,url_for,flash,Markup
 import uuid
 
 app = Flask(__name__)
@@ -15,14 +15,23 @@ def hello_world():
 @app.route("/setcookie",methods=['POST','GET'])
 def setcookie():
 
+
+    ## if already set "fpid" cookie, then use cookie value
     fpid = request.cookies.get('fpid', None)
+
+    fpid_query_param = request.args.get('fpid', None)
+
+    if(fpid_query_param != None):
+        fpid = fpid_query_param
+
     if(fpid == None):
 
         ## generate a unique id
         fpid = str(uuid.uuid4())
 
+    contents = Markup("FPID: " + fpid + "<p>if use 'fpid' query param, then use query param value as FPID(override)</p>")
     
-    resp = make_response(render_template('index.html',title="FPIDをセットします",contents=fpid))
+    resp = make_response(render_template('index.html',title="FPIDをセットします",contents=contents))
     resp.set_cookie('fpid',
         value=fpid,
         max_age=60*60*24*30*13,
